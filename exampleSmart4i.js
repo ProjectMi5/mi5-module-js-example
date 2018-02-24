e64bdd491d98b3f01317056e2ef9340efd2b1279
Dominik Serve <serve@fs.tum.de>
2018-02-24 15:58:47 +0100

    add example for new server default

---
+++ exampleSmart4i.js
@@ -0,0 +1,32 @@
+const Mi5ModuleJS = require('mi5-module');
+Mi5ModuleJS.setDefaults("smart4i");
+const Mi5Module = Mi5ModuleJS.Mi5Module;
+
+let module1 = new Mi5Module('Robot',undefined, {port: 4842});
+let module2 = new Mi5Module('Car',undefined, {port: 4842});
+let skill1 = module1.addSkill('skill1');
+let skill2 = module1.addSkill('skill2');
+let param1 = skill1.addInputParameter("distance", "Double", 2);
+let param2 = skill1.addInputParameter("height", "Double", 2);
+let param3 = skill1.addOutputParameter("weight", "Double", 2);
+let variab = module1.addStateVariable("weight", "Double", 2);
+
+
+
+module1.on("Idle", function(){
+  console.log('You are now in idle mode');
+});
+module1.on("Resetting", function(){
+  module1.done();
+});
+
+module1.onAny(function(event, value) {
+  console.log('module1 event: '+event, ' value: '+value);
+});
+
+skill1.onAny(function(event, value) {
+  console.log('skill1 event: '+event, ' value: '+value);
+  let number = param1.getValue();
+  console.log('Parameter 1:' + number);
+  param1.setValue(number+1);
+});
